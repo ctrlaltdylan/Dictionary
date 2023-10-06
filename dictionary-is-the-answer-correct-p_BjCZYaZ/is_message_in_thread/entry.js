90bfd72e63d364845a66ec4bb07eecf7065355ab
pierce <pierce@pipedream.com>
2023-10-06 13:43:40 +0000

    commit before merging to production

--- dictionary-is-the-answer-correct-p_BjCZYaZ/is_message_in_thread/entry.js
+++ dictionary-is-the-answer-correct-p_BjCZYaZ/is_message_in_thread/entry.js
@@ -2,6 +2,10 @@
 export default defineComponent({
   async run({ steps, $ }) {
     console.log(steps.get_thread_ts.$return_value, steps.trigger.event.thread_ts )
+
+    if(!steps.get_thread_ts.$return_value) {
+      $.flow.exit('No active thread. No game started yet.')
+    }
     // Return data to use it in future steps
     if(steps.trigger.event.thread_ts == steps.get_thread_ts.$return_value) {
       return true
