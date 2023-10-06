afa2a28182deeb9ecee6a638d607ac86bb630d2b
pierce <pierce@pipedream.com>
2023-10-06 14:13:52 +0000

    commit before merging to production

--- dictionary-hint-p_6lCBWdn/slack_bot/entry.js
+++ dictionary-hint-p_6lCBWdn/slack_bot/entry.js
@@ -7,7 +7,7 @@ export default defineComponent({
     },
   },
   async run({steps, $}) {
-    const currentPrompt = steps.get_all_records.$return_value.prompts.find((p => p.value === steps.get_all_records.$return_value.current_prompt))
+    const currentPrompt = steps.get_all_records.$return_value.all_available_prompts.find((p => p.value === steps.get_all_records.$return_value.current_prompt))
     return await axios($, {
       url: `https://slack.com/api/chat.postMessage`,
       headers: {
